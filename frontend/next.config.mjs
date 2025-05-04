0048cdb242fad97ae3e2ea915ac96f9a7d93b1ac
Rachit panwar <rachitpanwar0072@gmail.com>
2025-05-04 22:51:15 +0530

    Made changes to the configuration

---
+++ frontend/next.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+
+import NextPWA from "next-pwa";
+
+const withPWA = NextPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
+
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+};
+
+export default withPWA(nextConfig);
