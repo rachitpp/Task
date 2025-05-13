f3fb03646dfd3d2bc76e2bb3fe50c88caabbdfa5
Rachit panwar <rachitpanwar0072@gmail.com>
2025-05-13 17:31:13 +0530

    Changes

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -15,7 +15,6 @@ const userSchema = new mongoose.Schema(
     email: {
       type: String,
       required: [true, "Please enter your email"],
-      unique: true,
       lowercase: true,
       trim: true,
       match: [/^\S+@\S+\.\S+$/, "Please enter a valid email"],
